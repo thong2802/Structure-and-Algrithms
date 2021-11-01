5187d3c3e9e2fe0ecbd5975827daf00f209382ff
thong2802 <ducthong2802@gmail.com>
2021-11-01 15:33:50 +0700

    leetcode

---
+++ src/array/_1413MinimumValuetoGetPositiveStepbyStepSum.java
@@ -0,0 +1,11 @@
+package array;
+
+public class _1413MinimumValuetoGetPositiveStepbyStepSum {
+    public int minStartValue(int[] nums) {
+
+        return -1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
