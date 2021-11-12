0b973cded6146ed1a33f21ddc7355bf107d089d9
thong2802 <ducthong2802@gmail.com>
2021-11-13 00:51:49 +0700

    commit

---
+++ src/array/Demo.java
@@ -0,0 +1,22 @@
+package array;
+import java.util.Scanner;
+public class Demo {
+    public static boolean checkNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap X");
+        int X = scanner.nextInt();
+        System.out.println("Nhap Y");
+        int Y= scanner.nextInt();
+        System.out.println("Nhap Z");
+        int Z = scanner.nextInt();
+
+        if (X < Y && Y < Z) {
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+       boolean Kq =  checkNumber();
+        System.out.println(Kq);
+    }
+}
