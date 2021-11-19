2f9709ada60ad2906d7844ab0b3c3c4d6d293811
thong2802 <ducthong2802@gmail.com>
2021-11-19 11:52:11 +0700

    update

--- src/StackAndQueue/testMain.java
+++ src/StackAndQueue/testMain.java
@@ -2,7 +2,7 @@ package StackAndQueue;
 
 public class testMain {
     public static void main(String[] args) {
-        Stack_Array stack = new Stack_Array(3);
+/*        Stack_Array stack = new Stack_Array(3);
         System.out.println(stack.isEmpty());
         System.out.println(stack.push(1));
         System.out.println(stack.push(2));
@@ -11,6 +11,15 @@ public class testMain {
         System.out.println(stack.isFull());
         System.out.println(stack.pop());
         stack.show();
-        System.out.println(stack.isEmpty());
-    }
+        System.out.println(stack.isEmpty());*/
+
+
+        Queue_Array queue = new Queue_Array(3);
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.isFull());
+        System.out.println(queue.push(1));
+        System.out.println(queue.push(2));
+        System.out.println(queue.push(3));
+        queue.show();
+   }
 }
