f9dd1838457cbca310626eb01b77395547519794
thong2802 <ducthong2802@gmail.com>
2021-11-22 11:00:30 +0700

    upadte

--- src/StackAndQueue/_933NumberofRecentCalls.java
+++ src/StackAndQueue/_933NumberofRecentCalls.java
@@ -4,12 +4,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class _933NumberofRecentCalls {
-    private Queue<Integer> myQueue = new LinkedList<>();
-    public int ping(int t) {
+    private static Queue<Integer> myQueue = new LinkedList<>();
+
+    public static int ping(int t) {
         myQueue.add(t);
         while (myQueue.peek() < (t-3000)){
             myQueue.remove();
         }
         return myQueue.size();
     }
+
 }
