3aeddd10e01aafbffcb8aa3a740500706839f2cc
thong2802 <ducthong2802@gmail.com>
2021-11-24 11:53:56 +0700

    add

---
+++ src/array/_1331RankTransformofanArray.java
@@ -0,0 +1,40 @@
+package array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1331RankTransformofanArray {
+    public static int[] arrayRankTransform(int[] arr) {
+        int[] temp = new int[arr.length];
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+        Arrays.sort(arr);
+        int k=1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])){
+                continue;
+            }else {
+                map.put(arr[i], k);
+                k++;
+            }
+        }
+        for (int i = 0; i < temp.length; i++) {
+            result[i] = map.get(temp[i]);
+        }
+        return result;
+    }
+    // 40 10 20 30
+    // sort
+    // 10 20 30 40
+    // Map<10, 1><20,2><30,3><40,4>
+    //complexit time O(NLogN)
+    //complexit space O(N)
+    public static void main(String[] args) {
+       int[] arr = {40,10,20,30};
+        System.out.println(Arrays.toString(arrayRankTransform(arr)));
+    }
+}
---
+++ src/array/_1337TheKWeakestRowsinaMatrix.java
@@ -0,0 +1,12 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1337TheKWeakestRowsinaMatrix {
+
+    public static void main(String[] args) {
+
+    }
+}
