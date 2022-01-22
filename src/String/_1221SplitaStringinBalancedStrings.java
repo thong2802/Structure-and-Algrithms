ee6f914f242af81831651115f334eea15afb11be
thong2802 <ducthong2802@gmail.com>
2022-01-22 22:14:18 +0700

    add

--- src/String/_1221SplitaStringinBalancedStrings.java
+++ src/String/_1221SplitaStringinBalancedStrings.java
@@ -20,6 +20,25 @@ public class _1221SplitaStringinBalancedStrings {
         }
       return count;
     }
+
+    public static int balancedStringSplit2(String s) {
+        int count = 0;
+        int countOfL = 0;
+        int countOfR = 0;
+        for (int i = 0; i < s.length() ; i++) {
+            if (s.charAt(i) == 'R'){
+                countOfR++;
+            }else if (s.charAt(i) == 'L'){
+                countOfL++;
+            }
+            if (countOfL == countOfR) {
+                count++;
+                countOfL = 0;
+                countOfR = 0;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         String s =  "RLRRRLLRLL";
         System.out.println( balancedStringSplit(s));
