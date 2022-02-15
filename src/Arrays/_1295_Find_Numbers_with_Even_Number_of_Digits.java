46b13982bf9da66091ba5c1f2ab4ad640fe166e7
thong2802 <ducthong2802@gmail.com>
2022-02-15 08:48:24 +0700

    add

--- src/Arrays/_1295_Find_Numbers_with_Even_Number_of_Digits.java
+++ src/Arrays/_1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -25,10 +25,10 @@ public class  _1295_Find_Numbers_with_Even_Number_of_Digits {
         return count;
     }
 
-    // cach khac
+    // solution 1
     public int findNumbers1(int[] nums) {
         int count = 0;
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i ++) {
             if(String.valueOf(nums[i]).length() % 2 == 0){
                 count ++;
             }
