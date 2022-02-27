641588c80be892be455ea3f48658233a8dde5dca
thong2802 <ducthong2802@gmail.com>
2022-02-27 08:50:19 +0700

    add

---
+++ src/Sort/InsertSort.java
@@ -0,0 +1,30 @@
+package Sort;
+
+public class InsertSort {
+    public static void printInsertSort(int no, int[] a) {
+        System.out.printf("%d: ", no);
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%d ", a[i]);
+        }
+        System.out.println(" ");
+    }
+    public static void Insert_Sort(int[] a)
+    {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            // chen a[i] vao day [0, i-1]
+            int ai = a[i];
+            int j = i -1;
+            while (j >= 0 && a[j] > ai) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j+1] = ai;
+            printInsertSort(i, a);
+        }
+    }
+    public static void main(String[] args) {
+        int[] a = {5, 9, 6, 2, 1, 0, 3, 4 };
+        Insert_Sort(a);
+    }
+}
