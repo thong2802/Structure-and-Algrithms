63ee7efd5398e1b153dfcbfedc686c56d54b20e9
thong2802 <ducthong2802@gmail.com>
2022-03-11 16:03:33 +0700

    add

--- src/Arrays/_387_FirstUniqueCharacterinaString.java
+++ src/Arrays/_387_FirstUniqueCharacterinaString.java
@@ -1,7 +1,7 @@
 package Arrays;
 
 public class _387_FirstUniqueCharacterinaString {
-    public static   int firstUniqChar(String s) {
+    public static int firstUniqChar(String s) {
        int c = s.length();
        for(char i = 'a'; i <= 'z'; i++) {
            int index = s.indexOf(i);
@@ -29,8 +29,27 @@ public class _387_FirstUniqueCharacterinaString {
         }
         return -1;
     }
+
+    public static int firstUniqChar2(String s) {
+        char[] c = s.toCharArray();
+        int[] count = new int[123];
+        for (int i = 0; i < c.length; i++) {
+            System.out.println((int)c[i]);
+            char d = c[i];
+            int index = (int)d;
+            count[index]++;
+        }
+        for (int i = 0; i < c.length; i++) {
+            char d = c[i];
+            int index = (int)d;
+            if (count[index] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        String s = "lleetcode";
-        System.out.println(firstUniqChar1(s));
+        String s = "loveleetcode";
+        System.out.println(firstUniqChar2(s));
     }
 }
